a91e4b81d69feb30de29f99932ef60023d41cff1
17Vector <131187562+17Vector@users.noreply.github.com>
2024-04-22 20:37:42 +0500

    Backend Profile
    Creating a backend for a user profile with the ability to change their data

--- Pages/profile.php
+++ Pages/profile.php
@@ -1,3 +1,60 @@
-<form action="../php/logout.php" method="post">
-    <button type="submit" id="logout_btn">Выйти</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        session_start();
+        $connectMySQL = new mysqli('localhost', 'root', 'root', 'stolovka');
+        $id = $_SESSION['id'];
+        $role = $_SESSION['role'];
+
+        if ($role != "user" || $_SESSION['id'] == "")
+        {
+            header("Location: http://localhost/Pages/auth.php");
+            die();
+        }
+        else {
+            $username = $connectMySQL -> query("SELECT `first_name` FROM `users` WHERE `id` = '$id'") -> fetch_assoc()['first_name'];
+            $phone_number = $connectMySQL -> query("SELECT `phone_number` FROM `users` WHERE `id` = '$id'") -> fetch_assoc()['phone_number'];
+            $address = $connectMySQL -> query("SELECT `address` FROM `users` WHERE `id` = '$id'") -> fetch_assoc()['address'];
+
+            echo "<div><h1>Name: " . $username . "</h1>";
+            echo "<h3>Phone: " . $phone_number . "</h3>";
+            echo "<h3>Address: " . $address . "</h3>";
+            echo "<h3>Role: " . $role . "</h3></div>";
+
+            echo "<form id='reg' style='display: none;' action='../php/userDataChange.php' method='post'>
+                    <input type='text' name='edit_f_name' style='margin-bottom: 5px;' value='$username'></br>
+                    <input type='text' name='edit_add' style='margin-bottom: 5px;' value='$address'></br>
+                    <input type='submit' name='edit_btn' style='margin-bottom: 5px;' value='Внести изменения'>
+                </form>"; 
+        }
+    ?>
+    <div style='margin-bottom: 5px;'><button id="show_form" style="display:block;" onclick="HideForm(0)">Изменить данные</button>
+    <button id="hide_form" style="display:none;" onclick="HideForm(1)">Скрыть</button></div>
+    
+    <div style='margin-bottom: 5px;'><form action="../php/logout.php" method="post">
+        <button type="submit" id="logout_btn">Выйти</button>
+    </form></div>
+
+    <script>
+        function HideForm(bool) {
+            if (bool == 0)
+            {
+                document.getElementById('reg').style.display='block';
+                document.getElementById('show_form').style.display='none';
+                document.getElementById('hide_form').style.display='block';
+            }
+            else {
+                document.getElementById('reg').style.display='none';
+                document.getElementById('show_form').style.display='block';
+                document.getElementById('hide_form').style.display='none';
+            }
+        }
+    </script>
+</body>
+</html>
---
+++ php/userDataChange.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+
+    $name = $_POST['edit_f_name'];
+    $address = $_POST['edit_add'];
+    $id = $_SESSION['id'];
+
+    $connectMySQL = new mysqli('localhost', 'root', 'root', 'stolovka');
+
+    $sql_query = "UPDATE users SET first_name = ?, address = ? WHERE id=?";
+    $query = $connectMySQL -> prepare($sql_query);
+    $query -> bind_param("ssi", $name, $address, $id);
+    $query->execute();
+    
+    header("Location: http://localhost/Pages/profile.php");
+    die();
+?>
