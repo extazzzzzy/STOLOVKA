f0a1ccdf92b5f518611e6a86d626b43badd7691c
danyakovalugrasu <danyakovalugrasu@gmail.com>
2024-04-22 18:22:41 +0500

    fix

--- php/authorization.php
+++ php/authorization.php
@@ -2,7 +2,7 @@
 session_start();
 const DB_HOST = 'localhost';
 const DB_USER = 'root';
-const DB_PASSWORD = '';
+const DB_PASSWORD = 'root';
 const DB_NAME = 'stolovka';
 
 $mysql = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
